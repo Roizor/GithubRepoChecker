36072829f96c679fc63e12b910848596b1c997be
Roi <68617740+Roizor@users.noreply.github.com>
2023-04-30 20:52:03 -0500

    vb

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+using System.Collections;
+
+public class GithubRepoChecker
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Checking directory for directories w/ git repos");
+        IEnumerable<string> enumDir = Directory.EnumerateDirectories(args[0]);
+        foreach (string dir in enumDir)
+        {
+            IEnumerable<string> enumGitFolder = Directory.EnumerateDirectories(dir);
+            if (enumGitFolder.Contains(".git"))
+            {
+                Console.WriteLine(dir + "has a git repo");
+            } else
+            {
+                Console.WriteLine(dir + "does not have a git repo");
+            }
+        }
+    }
+}
